72f45bf3bfceb6cdbf0cb5fe3e4df3b8081030b0
Marcel Bührig <marcel@buehrig.me>
2015-07-17 11:33:45 +0200

    Removed not necessary close handlers
    Removed the close handlers when ("esc") was clicked an when the overlay selector was clicked.

--- src/nerdbox.js
+++ src/nerdbox.js
@@ -127,11 +127,7 @@ Nerdbox.prototype._setup = function() {
   }
 
   // Close lightbox
-  jQuery(this._overlaySelector()).on('click', Nerdbox.close);
   jQuery(this.options.nerdboxSelector).on('click', this.options.closeSelector, Nerdbox.close);
-  jQuery('body').on('keyup', function(event) {
-    if( event.keyCode == 27 ) { Nerdbox.close(); }
-  });
 
   this._trigger('initialized');
 };
